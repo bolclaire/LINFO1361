99c84107e24fc2a435e7db2d15c9b2162bbdd844
Sinnao <119690892+Sinnao@users.noreply.github.com>
2025-03-11 13:23:19 +0100

    Update gardener.py

--- gardener/gardener.py
+++ gardener/gardener.py
@@ -1,8 +1,44 @@
-from pycsp3 import *
+xfrom pycsp3 import *
 
 
 def solve_gardener(instructions: list[list[int]]) -> list[list[int]]:
     # Put your code here
+    n = len(instructions[0])
+    X = VarArray(size=[n, n], dom=range(1, n+1))
+
+    satisfy(
+        [AllDifferent(row) for row in X],
+        [AllDifferent(col) for col in columns(X)]
+    )
+
+    # from top
+    top = VarArray(size=[n-1, n], dom=range(0, 2))
+    satisfy(
+        *[[top[i-1,j] == (Maximum(X[:i, j]) < X[i,j]) for j in range(n)] for i in range(1,n)],
+        [Sum(top[:, i]) == (instructions[0][i]-1) for i in range(n)]
+    )
+
+    # from left
+    left = VarArray(size=[n, n-1], dom=range(0, 2))
+    satisfy(
+        *[[left[i,j-1] == (Maximum(X[i, :j]) < X[i,j]) for j in range(1,n)] for i in range(n)],
+        [Sum(left[i, :]) == (instructions[1][i]-1) for i in range(n)]
+    )
+
+    right = VarArray(size=[n, n-1], dom=range(0, 2))
+    satisfy(
+        *[[right[i,j] == (Maximum(X[i, j+1:]) < X[i,j]) for j in range(n-1)] for i in range(n)],
+        [Sum(right[i, :]) == (instructions[2][i]-1) for i in range(n)]
+    )
+
+    down = VarArray(size=[n-1, n], dom=range(0, 2))
+    satisfy(
+        *[[down[i,j] == (Maximum(X[i+1:, j]) < X[i,j]) for j in range(n)] for i in range(n-1)],
+        [Sum(down[:, i]) == (instructions[3][i]-1) for i in range(n)]
+    )
+
+    if solve(solver=CHOCO) is SAT:
+        return values(X)
     
     return None
 
