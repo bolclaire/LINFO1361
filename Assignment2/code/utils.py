0edebdf8cc554ddc0d4adccb5d93393cb30f9730
Claire Boland <claire.boland@student.uclouvain.be>
2025-04-06 23:29:11 +0200

    Rename

--- Assignment2/code/utils.py
+++
@@ -1,8 +0,0 @@
-from fenix import *
-from agent import *
-from random_agent import *
-from game_manager import *
-from visual_game_manager import *
-
-from alphabeta import *
-from mcts_agent2 import *
