52c0725b5d4041936a4d58d7f5799704d9e1d55b
Claire Boland <claire.boland@student.uclouvain.be>
2025-04-07 10:26:24 +0200

    My main

---
+++ Assignment2/code/main_cl.py
@@ -0,0 +1,31 @@
+from random_agent import RandomAgent
+from mcts_agent import MCTS
+from alphabeta import AlphaBetaAgent
+from visual_game_manager import VisualGameManager
+from game_manager import TextGameManager
+from some_heuristics import H1, H2
+
+
+agent1 = [AlphaBetaAgent(1, H1), AlphaBetaAgent(1, H2), MCTS(1, 1.414213)]
+agent2 = [AlphaBetaAgent(-1, H1), AlphaBetaAgent(-1, H2), MCTS(-1, 1.414213)]
+
+n = 3
+counts = [0]*n
+wins = [0]*n
+
+while True:
+    for i in range(n-1) :
+        for j in [n-1] :
+            if j != i :
+                l = [0]*n
+                l[i] = 1
+                l[j] = 1
+                print("Match: " + str(l))
+                v,w = TextGameManager(agent1[i], agent2[j], display=False).play()
+                counts[i] += 1
+                counts[j] += 1
+                wins[i] += (v+1)==2
+                wins[j] += (w+1)==2
+                print("Games: " + str(counts))
+                print("Wins : " + str(wins))
+                print("=================")
